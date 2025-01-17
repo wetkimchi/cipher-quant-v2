8d41499e39e79bba933ab2d11aa0e19806cea258
Dev Kalra <dev10kalra@gmail.com>
2025-01-17 13:42:20 +0400

    remove dry

--- src/dry.ts
+++
@@ -1,9 +0,0 @@
-import "./setup";
-import { mainStore } from "./store";
-
-async function main() {
-  const store = mainStore.get("B7D6yG93e4dNArwQ7SWJFwEbd7z2uBNSuji8oY9Hpump");
-  console.log(store?.strategiesLastAlertTime?.["High_Conviction_Alerts"]);
-}
-
-main();
