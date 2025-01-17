f629ed6b74b5c990765d0c619e7c9c046f09da0d
Dev Kalra <dev10kalra@gmail.com>
2025-01-17 23:03:16 +0400

    push script changes

--- run.sh
+++ run.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+cleanup() {
+    # Kill all child processes
+    pkill -P $$
+    exit
+}
+
+# Set up trap to catch SIGTERM and SIGINT
+trap cleanup SIGTERM SIGINT
+
 while true; do
     echo "[$(date)] Starting npm process..."
-    npm run start
+    npm run start &
+    NPM_PID=$!
     
+    # Wait for the npm process
+    wait $NPM_PID
     EXIT_CODE=$?
+    
     echo "[$(date)] Process exited with code: $EXIT_CODE"
+    
+    # Kill any remaining child processes
+    pkill -P $$
     
     echo "[$(date)] Restarting in 5 seconds..."
     sleep 5
