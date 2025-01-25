9c44c56273aedb99c1ef39771d1c1d93ad387610
bike4sail <bike4sail@gmail.com>
2025-01-25 00:38:21 -0600

    testing 15m timer on smart follows - it was spamming
    might have to revert if this doesnt work

--- src/strategies/smart-followers.ts
+++ src/strategies/smart-followers.ts
@@ -4,11 +4,18 @@ const ALERT_CHANNEL_ID = "1323921538331836446";
 const DISPLAY_NAME = "Smart_Followers";
 
 function areConditionsValidForAlert(_: string, details: AddressDetails) {
+  const lastAlertTime =
+    details.strategiesLastAlertTime?.[DISPLAY_NAME];
+
+  // last alert time should be more than 15 minutes ago
+  if (lastAlertTime && Date.now() - lastAlertTime < 60 * 15 * 1000)
+    return false;
+
   const moniXMentions = details.mentions.filter(
     (mention) => mention.channelId === MoniXSmartAlphaChannel.channelId
   );
 
-  return moniXMentions.length >= 3;
+  return moniXMentions.length >= 2;
 }
 
 function filterMentions(mentions: Mention[]) {
