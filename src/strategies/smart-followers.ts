c6ad2375e1c404273dac3261d0acef29a31f09fe
bike4sail <bike4sail@gmail.com>
2025-01-25 10:24:08 -0600

    Update smart-followers.ts

--- src/strategies/smart-followers.ts
+++ src/strategies/smart-followers.ts
@@ -4,18 +4,14 @@ const ALERT_CHANNEL_ID = "1323921538331836446";
 const DISPLAY_NAME = "Smart_Followers";
 
 function areConditionsValidForAlert(_: string, details: AddressDetails) {
-  const lastAlertTime =
-    details.strategiesLastAlertTime?.[DISPLAY_NAME];
-
-  // last alert time should be more than 15 minutes ago
-  if (lastAlertTime && Date.now() - lastAlertTime < 60 * 15 * 1000)
-    return false;
-
   const moniXMentions = details.mentions.filter(
+    (mention) =>
+      mention.channelId === MoniXSmartAlphaChannel.channelId &&
+      Date.now() - mention.timestamp <= 60 * 60 * 1000
     (mention) => mention.channelId === MoniXSmartAlphaChannel.channelId
   );
 
-  return moniXMentions.length >= 2;
+  return moniXMentions.length >= 3;
 }
 
 function filterMentions(mentions: Mention[]) {
